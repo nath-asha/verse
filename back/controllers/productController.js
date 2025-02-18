cfce5bc5be905f5fef70bd33ef2296782bcf5b7e
nath-asha <omthasha@gmail.com>
2025-02-18 12:08:04 +0530

    controllers

---
+++ back/controllers/productController.js
@@ -0,0 +1,20 @@
+const Product = require('../models/productModel');
+const asyncHandler = require('express-async-handler');
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
+
+module.exports = { getProducts, getProductById };
